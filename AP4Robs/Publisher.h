f6b44a708977c4fc1f2bdbafffaa2e701c1ea969
EinWolfNamensMenschgang <106486054+EinWolfNamensMenschgang@users.noreply.github.com>
2024-01-26 14:43:34 +0100

    Delete AP4Robs directory

--- AP4Robs/Publisher.h
+++
@@ -1,9 +0,0 @@
-#ifndef PUBLISHER_H
-#define PUBLISHER_H_
-#include <string>
-
-namespace Publisher{
-void publish(const char* serverIP, std::string twist_msg);
-}
-
-#endif // End of the file...
--- AP4Robs/Subscriber.h
+++
@@ -1,9 +0,0 @@
-#ifndef SUBSCRIBER_H
-#define SUBSCRIBER_H
-#include <vector>
-#include <string>
-namespace Subscriber{
-void DieWithError(const char *errorMessage);
-void subscribe(const char* serverIP, int port, int messageSize, std::vector<char> &returnString);
-}
-#endif 
