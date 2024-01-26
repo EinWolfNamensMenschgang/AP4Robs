bd308292b0ea312e34426c09b03f84719b48fe29
EinWolfNamensMenschgang <106486054+EinWolfNamensMenschgang@users.noreply.github.com>
2024-01-26 14:45:17 +0100

    Delete Subscriber.h

--- Subscriber.h
+++
@@ -1,9 +0,0 @@
-#ifndef SUBSCRIBER_H
-#define SUBSCRIBER_H
-
-#include <vector>
-namespace Subscriber{
-void DieWithError(const char *errorMessage);
-void subscribe(int port, int messageSize, std::vector<char> &returnString);
-}
-#endif 
